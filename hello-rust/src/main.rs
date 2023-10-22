6c3e70c025739d2d163024f9eb55997c90f05f4f
Michael Trang <michael.trang@hotmail.com>
2023-10-22 11:33:00 +0000

    explore unwrap

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,11 +1,21 @@
 use std::io;
+
 fn main() {
-    println!("Please enter you name: ");
-    let mut name = String::new();
-    io::stdin().read_line(&mut name);
-    say_name(&name);
+    println!("Please enter your first number: ");
+    let mut first = String::new();
+    io::stdin().read_line(&mut first);
+
+    let a: u32 = first.trim().parse().unwrap();
+
+    println!("Please enter your second number: ");
+    let mut second = String::new();
+    io::stdin().read_line(&mut second);
+
+    let b: u32 = second.trim().parse().unwrap();
+    let result = sum(a, b);
+    println!("{} + {} = {}", a, b, result);
 }
 
-fn say_name(name: &String) {
-    println!("Hello, {}", name);
+fn sum(first: u32, second: u32) -> u32 {
+    first + second
 }
