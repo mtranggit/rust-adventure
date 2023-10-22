638749909f3b088b0373e96f68cb683c4d2a6188
Michael Trang <michael.trang@hotmail.com>
2023-10-22 11:17:19 +0000

    explore variable borrowing

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,9 +1,11 @@
 fn main() {
     let first = "Michael".to_string();
     let last = "Trang".to_string();
-    say_name(first, last);
+    say_name(&first, &last);
+    say_name(&first, &last);
+    say_name(&first, &last);
 }
 
-fn say_name(first: String, last: String) {
+fn say_name(first: &String, last: &String) {
     println!("Hello, {} {}!", first, last);
 }
