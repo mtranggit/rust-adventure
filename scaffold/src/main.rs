60e32cd184bd7a7a17c5eec5da6c07e9976762a8
Michael Trang <michael.trang@hotmail.com>
2023-10-23 11:46:25 +0000

    adding comment to clap

--- scaffold/src/main.rs
+++ scaffold/src/main.rs
@@ -1,19 +1,27 @@
 use clap::Parser;
 
+/// Scaffold a new post for your blog
 #[derive(Parser, Debug)]
 struct Args {
+    /// The layout the post should use
     #[clap(short, long, default_value = "post")]
     layout: String,
 
+    /// Tags to include
     #[clap(short, long = "tag")]
     tags: Vec<String>,
 
+    /// The title of the post
+    ///
+    /// If not provided, the filename will be generated
     #[clap(short = 'T', long, default_value = "A post")]
     title: String,
 
+    /// Should this post be published?
     #[clap(short, long, default_value = "draft")]
     status: String,
 
+    /// Location to put the file
     #[clap(short, long, default_value = "content")]
     output_dir: String,
 }
