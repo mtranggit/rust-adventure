93ad536a2166c940b92d21eba36d736ca4f1eab7
Michael Trang <michael.trang@hotmail.com>
2023-10-26 12:04:46 +0000

    add new crate for csv upload

---
+++ planetscale-csv-upload/crates/upload-pokemon-data/src/main.rs
@@ -0,0 +1,8 @@
+fn main() -> Result<(), csv::Error> {
+    let mut reader = csv::Reader::from_path("./crates/upload-pokemon-data/pokemon.csv")?;
+    for result in reader.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
