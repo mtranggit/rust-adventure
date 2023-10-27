936aad050f22e8ac9aaae84fd6b6c5deeeaf31d8
Michael Trang <michael.trang@hotmail.com>
2023-10-27 12:20:54 +0000

    add module pokemon_csv

--- planetscale-csv-upload/crates/upload-pokemon-data/src/main.rs
+++ planetscale-csv-upload/crates/upload-pokemon-data/src/main.rs
@@ -1,7 +1,10 @@
+mod pokemon_csv;
+use pokemon_csv::*;
+
 fn main() -> Result<(), csv::Error> {
-    let mut reader = csv::Reader::from_path("./crates/upload-pokemon-data/pokemon.csv")?;
-    for result in reader.records() {
-        let record = result?;
+    let mut rdr = csv::Reader::from_path("./crates/upload-pokemon-data/pokemon.csv")?;
+    for result in rdr.deserialize() {
+        let record: PokemonCsv = result?;
         println!("{:?}", record);
     }
     Ok(())
