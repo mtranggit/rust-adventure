da5cf832427486a0b88c2d939d046c48a1979665
Michael Trang <michael.trang@hotmail.com>
2023-11-04 21:00:00 +0000

    update devcontainer projects

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -2,13 +2,68 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn exploration() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 10,
+        };
+
+        let smaller = Rectangle {
+            width: 5,
+            height: 2,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 10,
+        };
+
+        let smaller = Rectangle {
+            width: 5,
+            height: 2,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+    // #[test]
+    // // fn another() {
+    //     panic!("Make this test fail");
+    // }
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
     }
 }
