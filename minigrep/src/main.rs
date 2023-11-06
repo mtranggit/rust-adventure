51298c8c403bfb3c474b0ea37a4c3a2ccf9e7ad4
Michael Trang <michael.trang@hotmail.com>
2023-11-06 12:19:32 +0000

    refactor with if let

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, process};
+use std::{env, error::Error, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,7 +10,10 @@ fn main() {
     println!("Search for {}", config.query);
     println!("In file {}", config.file_path);
 
-    run(config);
+    if let Err(err) = run(config) {
+        println!("Application error: {err}");
+        process::exit(1);
+    };
 }
 
 struct Config {
@@ -30,9 +33,10 @@ impl Config {
     }
 }
 
-fn run(config: Config) {
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
     println!("With text \n{}", contents);
+
+    Ok(())
 }
