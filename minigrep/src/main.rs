d2b629c7165b5ee93fab4d28c1819d4eb77af558
Michael Trang <michael.trang@hotmail.com>
2023-11-06 11:59:07 +0000

    refactor with new

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,7 +2,7 @@ use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Search for {}", config.query);
     println!("In file {}", config.file_path);
@@ -14,14 +14,19 @@ fn main() {
     // dbg!(args);
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Config { query, file_path }
-}
-
 struct Config {
     query: String,
     file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }
+    }
 }
