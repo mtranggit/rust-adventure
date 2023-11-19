6dbeed630e39a5416f8f5515618ded0a4f71aad0
Michael Trang <michael.trang@hotmail.com>
2023-11-19 00:49:37 +0000

    explore multiple producers by cloning the transmitter

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -4,6 +4,9 @@ use std::time::Duration;
 
 fn main() {
     let (tx, rx) = mpsc::channel();
+
+    let tx1 = tx.clone();
+
     thread::spawn(move || {
         let vals = vec![
             String::from("hi"),
@@ -11,6 +14,19 @@ fn main() {
             String::from("the"),
             String::from("spawned"),
             String::from("thread"),
+        ];
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
         ];
         for val in vals {
             tx.send(val).unwrap();
